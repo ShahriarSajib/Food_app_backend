27067d49f5c6d772afb6bb7f797df9b614f72e27
ShahriarSajib <sajibhossen516@gmail.com>
2025-11-10 23:18:26 +0600

    added updateUser feature

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,9 +1,9 @@
 const userModel = require("../models/userModel");
+
 //get user info
 const getUserController = async (req, res) => {
     try {
         //find user
-        console.log("User ID:", req.user.id);
         const user = await userModel.findById(req.user.id);
         //validation
         if(!user){
@@ -30,4 +30,41 @@ const getUserController = async (req, res) => {
     }
 }
 
-module.exports = { getUserController };
+//update user info
+const updateUserController = async (req, res) => {
+    try {
+        //find user
+        const user = await userModel.findById(req.user.id);
+        //validation
+        if(!user){
+            return res.status(404).send({
+                success: false,
+                message: "User not found"
+            });
+        }
+        //update user
+        const {name, phone, address} = req.body;
+        if(name) user.name = name;
+        if(phone) user.phone = phone;
+        if(address) user.address = address;
+        //saving user
+        await user.save();
+        res.status(200).send({
+            success: true,
+            message: "User info updated",
+            user
+        });
+    } catch (error) {
+        console.log("Update User Error:", error);
+        res.status(500).send({
+            success: false,
+            message: "Error in updating user info",
+            error
+        });
+    }
+};
+
+module.exports = { 
+    getUserController,
+    updateUserController
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { getUserController } = require('../controllers/userControllers');
+const { getUserController, updateUserController } = require('../controllers/userControllers');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 //routes
 router.get('/getUser', authMiddleware, getUserController);
-
+router.put('/updateUser', authMiddleware, updateUserController);
 module.exports = router;
