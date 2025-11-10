0426041bb5f90231028a835b7f1cab51079fe6ee
ShahriarSajib <sajibhossen516@gmail.com>
2025-11-10 23:04:14 +0600

    added getUser info feature in userController

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,10 +1,33 @@
+const userModel = require("../models/userModel");
 //get user info
-const getUserController = (req, res) => {
-    res.status(200).send({
-        success: true,
-        message: "User info fetched successfully",
-        user: req.user, // assuming req.user is populated by an authentication middleware
-    });
+const getUserController = async (req, res) => {
+    try {
+        //find user
+        console.log("User ID:", req.user.id);
+        const user = await userModel.findById(req.user.id);
+        //validation
+        if(!user){
+            return res.status(404).send({
+                success: false,
+                message: "User not found"
+            });
+        }
+        //hiding password 
+        user.password = undefined;
+        //response 
+        res.status(200).send({
+            success: true,
+            message: "User info fetched",
+            user
+        });
+    } catch (error) {
+        console.log("Get User Error:", error);
+        res.status(500).send({
+            success: false,
+            message: "Error in getting user info",
+            error
+        })
+    }
 }
 
 module.exports = { getUserController };
